cbfd51d47a96536a8f9026b2984894fa13d3c9ab
roman <roman.klyucharov@gmail.com>
2025-04-11 18:16:28 +0300

    Пейзажи Марса

--- main.py
+++ main.py
@@ -1,42 +1,60 @@
-from flask import Flask, request, redirect, send_from_directory
+from flask import Flask, request, redirect, send_from_directory, url_for
 
 app = Flask(__name__)
 
 
-@app.route('/load_photo', methods=['GET', "POST"])
+@app.route('/carousel', methods=['GET'])
 def choice():
-    if request.method == "POST":
-        img = request.files['file']
-        img.save('static/img/avatar.png')
-        return redirect("/load_photo", code=302)
-    else:
-        return (
-            f"""
+    return (
+        f"""
             <!DOCTYPE html>
             <html>
             <head>
-            <link rel="stylesheet" href="/static/css/bootstrap.min.css">
-            <title>Загрузка фотографии</title>
+            <link rel="stylesheet" href="{url_for('static', filename='css/bootstrap.min.css')}">
+            <script src="{url_for('static', filename='css/bootstrap.bundle.min.js')}"></script>
+            <title>Пейзажи Марса</title>
             </head>
             <body>
-            <div class="container mt-4">
-                <h1 class="text-center"><b>Загрузка фотографии</b></h1>
-                <h4 class="text-center"><b>для участия в миссии</b></h4>
-                <div class="alert alert-warning">
-                    <form action="/load_photo" class="d-flex flex-column align-items-center" method="post" enctype=multipart/form-data>
-                        <div class="mb-3">
-                            <label class="form-label">Приложите фотографию</label>
-                            <input type="file" class="form-control" name="file" accept="image/*" required/>
+            <div class="container mt-5" style="max-width: 800px;">
+                <h1 class="text-center mb-4">Пейзажи Марса</h1>
+                <div id="carouselExampleIndicators" class="carousel slide">
+                  <div class="carousel-indicators">
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                  </div>
+                  <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img class="d-block w-100" src="{url_for('static', filename='img/1.jpg')}">
                         </div>
-                        <img src="static/img/avatar.png" alt='' class="img-fluid mb-3 "/>
-                        <button type="submit" class="btn btn-primary">Отправить</button>
-                    </form>
+                        <div class="carousel-item">
+                            <img class="d-block w-100" src="{url_for('static', filename='img/2.jpg')}">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="d-block w-100" src="{url_for('static', filename='img/3.png')}">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="d-block w-100" src="{url_for('static', filename='img/4.png')}">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="d-block w-100" src="{url_for('static', filename='img/5.jpg')}">
+                        </div>
+                    </div>
+                  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                  </button>
+                  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                  </button>
                 </div>
+
             </div>
             </body>
             </html>
             """
-        )
+    )
 
 
 @app.route('/static/<path>')
